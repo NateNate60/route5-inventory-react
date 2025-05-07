7b9145947c3df7348ade0cad2c58a9f2828d3643
Nathan Lim <nathanlim2004@gmail.com>
2025-05-07 15:37:24 -0700

    Add InventorySearcher component

---
+++ src/app/types/Product.tsx
@@ -0,0 +1,13 @@
+export type Product = {
+    id: string,
+    type: "card" | "slab" | "sealed",
+    description: string,
+    condition: "d" | "hp" | "mp" | "lp" | "nm" | "sealed",
+    acquired_price: number,
+    sale_price: number,
+    quantity: number,
+    consignor_name: string,
+    consignor_contact: string,
+    sale_price_date: string,
+    sale_date: string
+}
