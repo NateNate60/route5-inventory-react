e714b953846ad55b1dc6af56ae86ce817f20367b
Nathan Lim <nathanlim2004@gmail.com>
2025-05-07 15:38:10 -0700

    Create homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
-import React from "react";
+"use client"
 
-import GreenTextButton from "./components/greenbutton"
-import BlueTextButton from "./components/bluebuttton"
-import OrangeTextButton from "./components/orangebutton"
+import React, { useState } from "react";
+
+import GreenTextButton from "./components/buttons/greenbutton"
+import BlueTextButton from "./components/buttons/bluebuttton"
+import OrangeTextButton from "./components/buttons/orangebutton"
+import InventorySearcher from "./components/InventorySearcher";
 import "./style.css"
+import TokenSetter from "./components/TokenSetter";
+import ProductDisplayer from "./components/ProductDisplayer";
+import {Product} from "./types/Product"
 
 export default function Home() {
+    const [items, setItems] = useState<Array<Product>>([])
     return (
         <div>
             <h1 className="page-title">Home Page</h1>
@@ -15,19 +22,27 @@ export default function Home() {
                         <td>
                             <GreenTextButton text="Buy products from customer"/>
                         </td>
-                    </tr>
-                    <tr>
                         <td>
                             <OrangeTextButton text="Sell products to customer"/>  
                         </td>
+                    </tr>
+                    <tr>
                         <td>
-                            <BlueTextButton text="See all inventory"/>
+                            <BlueTextButton text="Manage inventory"/>
+                        </td>
+                        <td>
+                            <TokenSetter />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <InventorySearcher onSubmit={(e: Product) => {setItems([e])}}/>
                         </td>
                     </tr>
                 </tbody>
-
             </table>
-            
+            <br />
+            <ProductDisplayer products={items}/>
            
         </div>
     );
