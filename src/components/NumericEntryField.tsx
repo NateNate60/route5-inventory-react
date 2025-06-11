3559d2f2ddee24f710bfdc7809d8a3ec6d2d190a
Nathan Lim <nathanlim2004@gmail.com>
2025-06-11 11:33:14 -0700

    Implement components

---
+++ src/components/NumericEntryField.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+interface NumericEntryFieldProps {
+    step: 1 | 0.1 | 0.01,
+    onChange: (value: number) => any,
+    value: number,
+    max?: number,
+    min?: number
+}
+
+export default function NumericEntryField ({step, onChange, value, min, max}: NumericEntryFieldProps) {
+    return <input type="number" step={step} value={value} onFocus={(event) => event.target.select()}
+            min={min} max={max}
+            onChange={(e) => {
+                let value = parseFloat(e.target.value)
+                if (isNaN(value)) {
+                    value = 0
+                }
+                onChange(value)
+            }}
+            className="editable numeric-input"/>
+}
---
+++ src/components/SingleProductDisplayer.tsx
@@ -0,0 +1,83 @@
+import { Product } from "@/types/Product";
+
+interface SingleProductDisplayerProps {
+    product?: Product,
+    editable: boolean
+}
+
+export default function SingleProductDisplayer ({product, editable}: SingleProductDisplayerProps) {
+    let offset = new Date().getTimezoneOffset()
+    if (product === undefined) {
+        return (
+            <></>
+        )
+    }
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th className="single-product-displayer">
+                        Item Name
+                    </th>
+                    <td colSpan={3} className="single-product-displayer">
+                        {product.description}
+                    </td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th className="single-product-displayer">
+                        Condition
+                    </th>
+                    <td className="single-product-displayer">
+                        {product.condition}
+                    </td>
+                    <th className="single-product-displayer">
+                        Quantity In Stock
+                    </th>
+                    <td className="single-product-displayer">
+                        {product.quantity}
+                    </td>
+                </tr>
+                <tr>
+                    <th className="single-product-displayer">
+                        Acquired Price
+                    </th>
+                    <td className="single-product-displayer">
+                        ${Math.round(product.acquired_price) / 100}
+                    </td>
+                    <th className="single-product-displayer">
+                        Acquired Date
+                    </th>
+                    <td className="single-product-displayer">
+                        {new Date(new Date(product.acquired_date).getTime() + offset * 60 * 1000).toISOString().slice(0, 10)}
+                    </td>
+                </tr>
+                <tr>
+                    <th className="single-product-displayer">
+                        Sale Price
+                    </th>
+                    <td className="single-product-displayer">
+                        ${Math.round(product.sale_price) / 100}
+                    </td>
+                    <th className="single-product-displayer">
+                        Sale Date
+                    </th>
+                    <td className="single-product-displayer">
+                        {new Date(new Date(product.sale_date).getTime() + offset * 60 * 1000).toISOString().slice(0, 10)}
+                    </td>
+                </tr>
+                <tr>
+                    <th className="single-product-displayer">
+                        Consignor
+                    </th>
+                    <td colSpan={3} className="single-product-displayer">
+                        {product.consignor_name}
+                        <br/>
+                        {product.consignor_contact}
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    )
+}
