026d73ff70b52cfe5912c020ab517b9ef31f47ea
Nathan Lim <nathanlim2004@gmail.com>
2025-06-11 11:33:27 -0700

    Back button CSS className in wrong place

--- src/components/buttons/backbutton.jsx
+++ src/components/buttons/backbutton.jsx
@@ -2,6 +2,8 @@ import WhiteTextButton from "./whitebutton.jsx"
 
 export default function BackButton () {
     return (
-        <WhiteTextButton text="Back" href=".." float="left" className="back-button"/>
+        <div className="back-button">
+            <WhiteTextButton text="Back" href=".." float="left" className="back-button"/>
+        </div>
     )
 }
