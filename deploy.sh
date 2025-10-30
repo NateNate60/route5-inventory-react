109d549d19c6f25d40d7c05fd4272816cfe77947
Nathan Lim <nathanlim2004@gmail.com>
2025-10-30 16:16:03 -0700

    Deploy with corrected constants.json URL

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
+mv src/constants.json src/constants1.json
+echo '{"BACKEND_URL": "https://api.routefivecollectibles.com"}' > src/constants.json
 npm run build
 rm /var/www/route5-react -r
 mv out /var/www/route5-react
+mv src/constants1.json src/constants.json
